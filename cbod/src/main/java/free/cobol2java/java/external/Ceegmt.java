d3a509056eaa54b191de972cec94e61595febfc8
lidonge <dongli72@gmail.com>
2026-04-09 14:41:46 +0800

    modify external class

--- cbod/src/main/java/free/cobol2java/java/external/Ceegmt.java
+++ cbod/src/main/java/free/cobol2java/java/external/Ceegmt.java
@@ -3,4 +3,6 @@ package free.cobol2java.java.external;
 import free.cobol2java.java.IService;
 
 public class Ceegmt implements IService {
+    public void procedure(Object wkAbsDate, Object wkAbsTime){ 
+    }
 }
--- cbod/src/main/java/free/cobol2java/java/external/Ceegmto.java
+++ cbod/src/main/java/free/cobol2java/java/external/Ceegmto.java
@@ -3,4 +3,6 @@ package free.cobol2java.java.external;
 import free.cobol2java.java.IService;
 
 public class Ceegmto implements IService {
+    public void procedure(Object wkAbsDate, Object wkAbsTime, Object wkTimeZone){ 
+    }
 }
